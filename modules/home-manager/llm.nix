47d0d9fc4ff764e09027a5f694fe2aaf904a8e32
Your Name <you@example.com>
2025-03-10 21:44:25 +0000

    feat: Add LLM module with Ollama support for Home Manager

--- modules/home-manager/llm.nix
+++ modules/home-manager/llm.nix
@@ -1,1 +1,28 @@
 
+{ config, lib, pkgs, unstable, ... }:
+
+with lib;
+
+let
+  cfg = config.modules.llm;
+  pyWithLlm = (
+    unstable.python313Packages.llm
+    unstable.python313Packages.llm-ollama
+  );
+  llm-with-plugins = (
+    pkgs.writeShellScriptBin "llm" ''
+      exec ${pyWithLlm}/bin/llm "$@"
+    ''
+  );
+in
+{
+  options.modules.llm = {
+    enable = mkEnableOption "LLM tools and configurations";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [
+      llm-with-plugins
+    ];
+  };
+}
